73c2b5df191587243eb666b391e73021fbdf0ee2
Ali Abbasi <ali_sitrick@yahoo.com>
2024-01-09 13:05:43 -0800

    added background color of deeppink to the calculator's functioning keys

--- src/App.js
+++ src/App.js
@@ -8,24 +8,24 @@ function App() {
           <input type="text"/>
         </form>
         <div className="keypad">
-          <button>AC</button>
-          <button>DEL</button>
-          <button>&divide;</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>&times;</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>&minus;</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <button id="clear" className="highlight">AC</button>
+          <button className="highlight">DEL</button>
+          <button className="highlight">&divide;</button>
+          <button onClick={handleClick}>7</button>
+          <button onClick={handleClick}>8</button>
+          <button onClick={handleClick}>9</button>
+          <button onClick={handleClick} className="highlight">&times;</button>
+          <button onClick={handleClick}>4</button>
+          <button onClick={handleClick}>5</button>
+          <button onClick={handleClick}>6</button>
+          <button onClick={handleClick} className="highlight">&minus;</button>
+          <button onClick={handleClick}>1</button>
+          <button onClick={handleClick}>2</button>
+          <button onClick={handleClick}>3</button>
+          <button onClick={handleClick} className="highlight">+</button>
+          <button onClick={handleClick}>0</button>
+          <button onClick={handleClick}>.</button>
+          <button onClick={handleClick} id="equal">=</button>
         </div>
       </div>
     </div>
