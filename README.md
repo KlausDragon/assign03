67aa153ebc0098c734ce35ed2d98664d639dcefb
Ali Abbasi <ali_sitrick@yahoo.com>
2024-01-11 10:13:03 -0800

    updated README

--- README.md
+++ README.md
@@ -1,70 +1,36 @@
-# Getting Started with Create React App
+# KlausDragon Calculator
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Introduction
+KlausDragon Calculator is a simple yet powerful web calculator built using React.js. It features basic arithmetic operations, memory functions, percentage calculations, and square root functionality.
 
-## Available Scripts
+## Features
+- Basic Arithmetic (Add, Subtract, Multiply, Divide)
+- Memory Functions (M+, M-, MC, MR, MS)
+- Additional Features (Percentage, Square Root, +/- Toggle)
+- Dynamic Year Display in Footer
 
-In the project directory, you can run:
+## Installation
+1. Clone the repository: 
+git clone https://github.com/KlausDragon/calculator-react-app
 
-### `npm start`
+2. Install dependencies: 
+cd [project folder]
+npm install
+npm install mathjs
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+3. Run the application: The app will launch in your default browser.
+npm start
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Usage
+- Click the buttons on the calculator to perform operations.
+- Use memory buttons to store and use values for later calculations.
+- The display shows the current operation or result.
 
-### `npm test`
+## Contributing
+Feel free to fork the repository and submit pull requests.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## License
+This project is licensed under the MIT License.
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## Credits
+Developed by Ali Abbasi - © ` + getCurrentYear() + `
--- src/App.js
+++ src/App.js
@@ -1,129 +1,190 @@
-import React, { useState } from 'react';
-import { evaluate } from 'mathjs';
+import React, { useState } from "react";
+import { evaluate } from "mathjs";
 
 function App() {
-  
-  const [total, setTotal] = useState('0');
-  
+  const [total, setTotal] = useState("0");
+
   function getCurrentYear() {
     const currentYear = new Date().getFullYear();
     return currentYear;
   }
-  
+
   const handleClick = (e) => {
     const value = e.target.name;
-    if (total === 'Error' || total === '0') {
+    if (total === "Error" || total === "0") {
       setTotal(value);
     } else {
       setTotal(total.concat(value));
     }
-  }
+  };
 
   const clear = () => {
-    setTotal('0');
-  }
+    setTotal("0");
+  };
 
   const handleDelete = () => {
     setTotal(total.slice(0, -1));
-  }
+  };
 
   const signNum = () => {
-    if (total !== '0') {
-      if (total[0] === '-') {
+    if (total !== "0") {
+      if (total[0] === "-") {
         setTotal(total.slice(1));
       } else {
-        setTotal('-'.concat(total));
+        setTotal("-".concat(total));
       }
     }
-  }
+  };
 
   const handleMemory = (e) => {
     const value = e.target.name;
-    if (value === 'MC') {
-      localStorage.removeItem('memory');
-    } else if (value === 'MR') {
-      if (localStorage.getItem('memory')) {
-        setTotal(evaluate(localStorage.getItem('memory')).toString());
+    if (value === "MC") {
+      localStorage.removeItem("memory");
+    } else if (value === "MR") {
+      if (localStorage.getItem("memory")) {
+        setTotal(evaluate(localStorage.getItem("memory")).toString());
       }
-    } else if (value === 'MS') {
-      localStorage.setItem('memory', total);
-    } else if (value === 'M+') {
-      if (localStorage.getItem('memory')) {
-        localStorage.setItem('memory', evaluate(localStorage.getItem('memory') + '+' + total));
+    } else if (value === "MS") {
+      localStorage.setItem("memory", total);
+    } else if (value === "M+") {
+      if (localStorage.getItem("memory")) {
+        localStorage.setItem(
+          "memory",
+          evaluate(localStorage.getItem("memory") + "+" + total)
+        );
       } else {
-        localStorage.setItem('memory', total);
+        localStorage.setItem("memory", total);
       }
-    } else if (value === 'M-') {
-      if (localStorage.getItem('memory')) {
-        localStorage.setItem('memory', evaluate(localStorage.getItem('memory') + '-' + total));
+    } else if (value === "M-") {
+      if (localStorage.getItem("memory")) {
+        localStorage.setItem(
+          "memory",
+          evaluate(localStorage.getItem("memory") + "-" + total)
+        );
       } else {
-        localStorage.setItem('memory', total);
+        localStorage.setItem("memory", total);
       }
     }
-  }
+  };
 
   const handlePercent = () => {
-    if (total !== '0') {
-      const newTotal = evaluate(total) / 100
+    if (total !== "0") {
+      const newTotal = evaluate(total) / 100;
       setTotal(newTotal.toString());
     }
-  }
+  };
 
   const handleSqrt = () => {
-    if (total !== '0') {
+    if (total !== "0") {
       setTotal(Math.sqrt(evaluate(total)).toString());
     }
-  }
+  };
 
   const calculate = () => {
-    try{
+    try {
       let result = evaluate(total).toString();
-      if(result == 'Infinity' || result == 'NaN'){
-        setTotal("Error")
+      if (result == "Infinity" || result == "NaN") {
+        setTotal("Error");
       } else {
         setTotal(result);
-      } 
-    } catch(e) {
-      setTotal('Error');
+      }
+    } catch (e) {
+      setTotal("Error");
     }
-  }
+  };
 
   return (
     <div className="App">
       <h1>KlausDragon Calculator</h1>
       <div className="container">
         <form>
-          <input type="text" value={total} readOnly/>
+          <input type="text" value={total} readOnly />
         </form>
         <div className="keypad">
-          <button onClick={clear} id="clear" className="highlight">AC</button>
-          <button onClick={handleDelete} className="highlight">DEL</button>
-          <button name='/' onClick={handleClick} className="highlight">&divide;</button>
-          <button name='7' onClick={handleClick}>7</button>
-          <button name='8' onClick={handleClick}>8</button>
-          <button name='9' onClick={handleClick}>9</button>
-          <button name='*' onClick={handleClick} className="highlight">&times;</button>
-          <button name='4' onClick={handleClick}>4</button>
-          <button name='5' onClick={handleClick}>5</button>
-          <button name='6' onClick={handleClick}>6</button>
-          <button name='-' onClick={handleClick} className="highlight">&minus;</button>
-          <button name='1' onClick={handleClick}>1</button>
-          <button name='2' onClick={handleClick}>2</button>
-          <button name='3' onClick={handleClick}>3</button>
-          <button name='+' onClick={handleClick} className="highlight">+</button>
-          <button name='0' onClick={handleClick}>0</button>
-          <button name='.' onClick={handleClick}>.</button>
-          <button name='=' onClick={calculate} id="equal">=</button>
-          <button name='MS' onClick={handleMemory} className='lowlight'>MS</button>
-          <button name='MC' onClick={handleMemory} className='lowlight'>MC</button>
-          <button name='MR' onClick={handleMemory} className='lowlight'>MR</button>
-          <button className='lowlight' onClick={signNum}>+/-</button>
-          <button name='M+' onClick={handleMemory} className='lowlight'>M+</button>
-          <button name='M-' onClick={handleMemory} className='lowlight'>M-</button>
-          <button name='%' onClick={handlePercent} className="lowlight">%</button>
-          <button onClick={handleSqrt} className="lowlight">&radic;</button>
-          <button name='(' onClick={handleClick} className="para">(</button>
-          <button name=')' onClick={handleClick} className="para">)</button>
+          <button onClick={clear} id="clear" className="highlight">
+            AC
+          </button>
+          <button onClick={handleDelete} className="highlight">
+            DEL
+          </button>
+          <button name="/" onClick={handleClick} className="highlight">
+            &divide;
+          </button>
+          <button name="7" onClick={handleClick}>
+            7
+          </button>
+          <button name="8" onClick={handleClick}>
+            8
+          </button>
+          <button name="9" onClick={handleClick}>
+            9
+          </button>
+          <button name="*" onClick={handleClick} className="highlight">
+            &times;
+          </button>
+          <button name="4" onClick={handleClick}>
+            4
+          </button>
+          <button name="5" onClick={handleClick}>
+            5
+          </button>
+          <button name="6" onClick={handleClick}>
+            6
+          </button>
+          <button name="-" onClick={handleClick} className="highlight">
+            &minus;
+          </button>
+          <button name="1" onClick={handleClick}>
+            1
+          </button>
+          <button name="2" onClick={handleClick}>
+            2
+          </button>
+          <button name="3" onClick={handleClick}>
+            3
+          </button>
+          <button name="+" onClick={handleClick} className="highlight">
+            +
+          </button>
+          <button name="0" onClick={handleClick}>
+            0
+          </button>
+          <button name="." onClick={handleClick}>
+            .
+          </button>
+          <button name="=" onClick={calculate} id="equal">
+            =
+          </button>
+          <button name="MS" onClick={handleMemory} className="lowlight">
+            MS
+          </button>
+          <button name="MC" onClick={handleMemory} className="lowlight">
+            MC
+          </button>
+          <button name="MR" onClick={handleMemory} className="lowlight">
+            MR
+          </button>
+          <button className="lowlight" onClick={signNum}>
+            +/-
+          </button>
+          <button name="M+" onClick={handleMemory} className="lowlight">
+            M+
+          </button>
+          <button name="M-" onClick={handleMemory} className="lowlight">
+            M-
+          </button>
+          <button name="%" onClick={handlePercent} className="lowlight">
+            %
+          </button>
+          <button onClick={handleSqrt} className="lowlight">
+            &radic;
+          </button>
+          <button name="(" onClick={handleClick} className="para">
+            (
+          </button>
+          <button name=")" onClick={handleClick} className="para">
+            )
+          </button>
         </div>
       </div>
       <div className="footer">
